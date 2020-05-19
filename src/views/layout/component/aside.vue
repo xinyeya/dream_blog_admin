84a8de3d82822989e1f32a88135b9f1f57f8f937
心叶 <1329132389@qq.com>
2020-05-19 17:07:36 +0800

    初始化侧边栏样式

--- src/views/layout/component/aside.vue
+++ src/views/layout/component/aside.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
-        <span>侧边栏</span>
+        <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#D3DCE6"
+            router>
+        <el-menu-item index="/">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">发表文章</span>
+        </el-menu-item>
+        <el-menu-item index="/article">
+            <i class="el-icon-document"></i>
+            <span slot="title">文章管理</span>
+        </el-menu-item>
+        <el-menu-item index="/class">
+            <i class="el-icon-menu"></i>
+            <span slot="title">分类管理</span>
+        </el-menu-item>
+        <el-menu-item index="/music">
+            <i class="el-icon-service"></i>
+            <span slot="title">音乐管理</span>
+        </el-menu-item>
+    </el-menu>
     </div>
 </template>
 
