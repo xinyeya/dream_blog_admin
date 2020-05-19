4dae2d801923075541a4fa36e511f3e6668462aa
心叶 <1329132389@qq.com>
2020-05-19 17:45:46 +0800

    初始化发表文章页面

--- src/main.js
+++ src/main.js
@@ -5,7 +5,15 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import VueQuillEditor from 'vue-quill-editor'
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 Vue.use(ElementUI);
+Vue.use(VueQuillEditor, /* { default global options } */)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,60 @@
 <template>
     <div>
-        <span>首页</span>
+        <el-card>
+            <el-row type="flex" justify="space-aroun" align="center">
+                <el-col :span="3">
+                    <el-button type="primary">保存草稿</el-button>
+                </el-col>
+                <el-col :span="3">
+                    <el-button type="success">发表文章</el-button>
+                </el-col>
+            </el-row>
+            <el-row>
+                <quillEditor v-model="content" ref="myQuillEditor" :options="editorOption" class="editor">
+                </quillEditor>
+            </el-row>
+        </el-card>
     </div>
 </template>
 
 <script>
+    // require styles
+    import 'quill/dist/quill.core.css'
+    import 'quill/dist/quill.snow.css'
+    import 'quill/dist/quill.bubble.css'
+
+    import { quillEditor } from 'vue-quill-editor'
+    import { addQuillTitle } from '@/utils/quill-title.js'
+
     export default {
-        name: "Home"
+        name: "Home",
+        components: {
+            quillEditor
+        },
+
+        data() {
+            return {
+                content: '',
+                editorOption: {
+                    placeholder: "请输入"
+                }
+            }
+        },
+
+        mounted() {
+            addQuillTitle();
+        },
+
+        methods () {
+        }
     }
 </script>
 
 <style scoped>
-
+    .el-card{
+        height: 90vh;
+    }
+    .editor{
+        height: calc(80vh - 100px);
+    }
 </style>
