c9f4ffecb2de0a6d50187897d76e6481bf336d9c
心叶 <1329132389@qq.com>
2020-05-19 17:01:46 +0800

    初始化layout布局页面

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,15 +1,51 @@
 <template>
     <div>
-        <span>框架布局</span>
+        <el-container>
+            <el-header>
+                <Header></Header>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <Aside></Aside>
+                </el-aside>
+                <el-main>
+                    <router-view/>
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
+    import Header from './component/header'
+    import Aside from './component/aside'
     export default {
-        name: "index"
+        name: "index",
+        components: {
+            Header,
+            Aside
+        }
     }
 </script>
 
 <style scoped>
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
 
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        height: 100vh;
+    }
 </style>
