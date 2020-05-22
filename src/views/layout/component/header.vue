9fe73db166947e039d6980f8086242a54421061b
心叶 <1329132389@qq.com>
2020-05-22 08:28:50 +0800

    初始化头部页面

--- src/views/layout/component/header.vue
+++ src/views/layout/component/header.vue
@@ -1,12 +1,41 @@
 <template>
     <div>
-        <span>头部</span>
+        <el-row type="flex" justify="space-between" align="center">
+            <el-col :span="3">
+                <img style="height: 60px;" src="@/assets/logo.png" alt="">
+            </el-col>
+            <el-col :span="3">
+                <el-dropdown>
+                    <span class="el-dropdown-link">
+                        <div class="block">
+                            <el-avatar :size="60" :src="circleUrl" @error="errorHandler">
+                                <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
+                            </el-avatar>
+                        </div>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人信息</el-dropdown-item>
+                        <el-dropdown-item>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        data () {
+            return {
+                circleUrl: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+            }
+        },
+        methods: {
+            errorHandler() {
+                return true
+            }
+        }
     }
 </script>
 
